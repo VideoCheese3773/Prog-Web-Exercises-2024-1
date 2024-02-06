488e6aef5349decf6dfca4d81728022be3940757
VideoCheese3773 <eedduxx@gmail.com>
2024-02-06 16:44:18 -0500

    added card jsx & css

--- web-prog-vite-proj/src/components/Body/Body.jsx
+++ web-prog-vite-proj/src/components/Body/Body.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './style.css'
 
+import { Card } from '../Card/Card'
+
 export function Body() {
     return (
         <main>
-            <h1>Body Go BRRRRRRRRRRRRRRRRRRRRRRR</h1>
+            <h1>Body Go BRRRRRRRRRR</h1>
+            <Card/>
             <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet et delectus, sint nulla architecto tenetur odit hic qui accusantium quae ut nostrum molestias, reiciendis laudantium. Dolor magni consequatur natus architecto.</h3>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum error nemo provident eveniet numquam labore, pariatur illum cum veniam eaque maxime dolorem tempore in. Optio delectus quaerat beatae expedita consequatur.</p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque corrupti velit repellendus cum, porro est quidem quis excepturi, amet vel beatae enim nostrum quos. Blanditiis soluta id nisi aliquam quisquam.</p>
---
+++ web-prog-vite-proj/src/components/Card/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+
+export function Card() {
+    return (
+        <div className='mainDiv'>
+            <img src="" alt="" />
+            <div className='textDiv'>
+                <h2 className='cardTitle'>
+                    Title
+                </h2>
+                <p className='cardDesc'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis adipisci aliquid dignissimos veritatis dolorem eius</p>
+            </div>
+        </div>
+    )
+}
