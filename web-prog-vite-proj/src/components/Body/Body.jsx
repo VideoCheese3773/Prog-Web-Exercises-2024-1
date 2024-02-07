ba2145bac4034b7bdb946bf86cecab0f5b34f1df
VideoCheese3773 <eedduxx@gmail.com>
2024-02-07 10:19:46 -0500

    added images, card CSS in progress

--- web-prog-vite-proj/src/components/Body/Body.jsx
+++ web-prog-vite-proj/src/components/Body/Body.jsx
@@ -7,7 +7,7 @@ export function Body() {
     return (
         <main>
             <h1>Body Go BRRRRRRRRRR</h1>
-            <Card/>
+                <Card />
             <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet et delectus, sint nulla architecto tenetur odit hic qui accusantium quae ut nostrum molestias, reiciendis laudantium. Dolor magni consequatur natus architecto.</h3>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum error nemo provident eveniet numquam labore, pariatur illum cum veniam eaque maxime dolorem tempore in. Optio delectus quaerat beatae expedita consequatur.</p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque corrupti velit repellendus cum, porro est quidem quis excepturi, amet vel beatae enim nostrum quos. Blanditiis soluta id nisi aliquam quisquam.</p>
--- web-prog-vite-proj/src/components/Card/Card.jsx
+++ web-prog-vite-proj/src/components/Card/Card.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
 import './style.css'
 
+import img1 from '../../images/photo-1.jpg'
+import img2 from '../../images/photo-2.jpg'
+import img3 from '../../images/photo-3.jpg'
+import img4 from '../../images/photo-4.jpg'
+import img5 from '../../images/photo-5.jpg'
+import img6 from '../../images/photo-6.jpg'
+import img7 from '../../images/photo-7.jpg'
+import img8 from '../../images/photo-8.jpg'
+import img9 from '../../images/photo-9.jpg'
+
+const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9]
+
 export function Card() {
     return (
-        <div className='mainDiv'>
-            <img src="" alt="" />
-            <div className='textDiv'>
-                <h2 className='cardTitle'>
-                    Title
-                </h2>
-                <p className='cardDesc'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis adipisci aliquid dignissimos veritatis dolorem eius</p>
-            </div>
+        <div className='cardContainer'>
+            {
+                images.map((images) =>
+                    <div className='mainDiv'>
+                        <img className='cardImg' src={images} alt="stockImage" />
+                        <div className='textDiv'>
+                            <h2 className='cardTitle'>
+                                Title
+                            </h2>
+                            <p className='cardDesc'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis adipisci aliquid dignissimos veritatis dolorem eius</p>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     )
 }
