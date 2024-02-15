e94ff001ef83999e24805c4198a116c6bf547f9a
VideoCheese3773 <eedduxx@gmail.com>
2024-02-14 19:27:46 -0500

    added hamburger menu

--- web-prog-vite-proj/src/components/Header/Header.jsx
+++ web-prog-vite-proj/src/components/Header/Header.jsx
@@ -1,15 +1,53 @@
+import { useState } from 'react';
 import './style.css'
+
+const links = [
+    {
+        id: crypto.randomUUID(),
+        text: "Link 1"
+    },
+    {
+        id: crypto.randomUUID(),
+        text: "Link 2"
+    },
+    {
+        id: crypto.randomUUID(),
+        text: "Link 3"
+    }
+]
+console.log(links);
+
+const [showMenu ,setShowMenu] = useState(false)
+
+console.log("state value", showMenu);
+
+const handleClick = (event) => {
+    setShowMenu(!showMenu)
+}
+
+//TODO Finish functionality of dropdown menu
 
 export function Header() {
     return (
         <header>
             <h2 className='headTitle pc'>Reputable Brand Page</h2>
             <h2 className='headTitle mobile'>RBP</h2>
-            <div className='btnContainer'>
-                <button className='headBtn'>Button 1</button>
-                <button className='headBtn'>Button 2</button>
-                <button className='headBtn'>Button 3</button>
-            </div>
+
+            <nav>
+                <ul className='btnContainer'>
+                    {
+                        links.map(({ id, text }) => {
+                            return (
+                                <li className='headBtn' key={id}>{text}</li>
+                            )
+                        })
+                    }
+                </ul>
+            </nav>
+
+            <button className='hamburgerMenu'>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Hamburger_icon.svg" alt="hamburger menu" />
+            </button>
         </header>
     )
 }
